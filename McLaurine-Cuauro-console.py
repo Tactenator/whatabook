4ebf9dd27e20281a2f629903c8602b576a8db7fb
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-07-23 17:34:26 -0400

    added customers retrieval code and change the db c

--- McLaurine-Cuauro-console.py
+++ McLaurine-Cuauro-console.py
@@ -1,12 +1,12 @@
-#Trevor McLaurine
-#7/19/2023
-#McLaurine_console.py
-#Web 335 - Assignment 9.2
+# Trevor McLaurine
+# 7/19/2023
+# McLaurine_console.py
+# Web 335 - Assignment 9.2
 
-#Importing MongoClient from pymongo
+# Importing MongoClient from pymongo
 import pymongo
 
-# Kept receiving error that stated certificate was invalid. Imported this to solve the problem. 
+# Kept receiving error that stated certificate was invalid. Imported this to solve the problem.
 import certifi
 
 print("*****************************************************")
@@ -15,24 +15,31 @@ print("*****************************************************")
 
 print("Connecting to MongoDB....\n")
 
-#Connection string to connect to MongoDB. Certifi fixes certification issues
-client = pymongo.MongoClient("mongodb+srv://web335_user:s3cret@web340db.93lxfky.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+# Connection string to connect to MongoDB. Certifi fixes certification issues
+client = pymongo.MongoClient(
+    # "mongodb+srv://web335_user:s3cret@web340db.93lxfky.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where()
+    # Trevor's Database connection string
+    "mongodb+srv://web335_user:s3cret@bellevueuniversity.5jww2it.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where()
+    # Patrick's Database connection string
+)
 
-if(client):
+if (client):
     print("Success!\n")
 
-#connect to web335DB
-db = client['web335DB']
+# connect to web335DB
+# db = client['web335DB']
+# connect to Patricks DB
+db = client['Whatabook']
 
 print("Connecting to database...\n")
 
-#initiates the collection to use
+# initiates the collection to use
 books = db['books']
 customers = db['customers']
 
 print("Database connected. Retrieving book list...\n")
 
-bookList = db.books.find({ })
+bookList = db.books.find({})
 
 for document in bookList:
     print("BookID: " + str(document.get('bookId')))
@@ -40,35 +47,94 @@ for document in bookList:
     print("Genre: " + str(document.get('genre')))
     print("Author: " + str(document.get('author')))
     print("\n ******************************************************* \n")
+# included by Patrick
+# def retrieve_array(object):
+#   """Retrieves an array from an object.
+
+#   Args:
+    # object: The object that contains the array.
+
+#   Returns:
+    # The array.
+#   """
+
+#   array = object.get("array")
+#   if not array:
+#     raise ValueError("The object does not contain an array.")
+
+#   return array
+
+
+# if __name__ == "__main__":
+#   object = {"array": [1, 2, 3]}
+#   array = retrieve_array(object)
+#   print(array)
+#   end of included by Patrick
 
 run_app = True
 print("How can we help you today?\n To search by genre, type 'genre'. To display a customer's wishlist, type 'wishlist' .")
 print("If you wish to exit, type 'exit' ")
 
-while(run_app):
+while (run_app):
     user_input = input("What would you like to do?: ")
-    if(user_input == "genre"):
+    if (user_input == "genre"):
         print("Please choose from fantasy, fiction, science fiction, or historical fiction")
         genre_input = input("What Genre would you like to search for?: ")
-        if(genre_input == ""):
+        if (genre_input == ""):
             print("Input is required.")
         else:
             print("Searching for " + genre_input + " books")
-            books_found = (db.books.find({ 'genre': genre_input }))
+            books_found = (db.books.find({'genre': genre_input}))
             for document in books_found:
-                 print("\n ******************************************************* \n")
-                 print("BookID: " + str(document.get('bookId')))
-                 print("Title: " + str(document.get('title')))
-                 print("Genre: " + str(document.get('genre')))
-                 print("Author: " + str(document.get('author')))
-                 print("\n ******************************************************* \n")
-    elif(user_input == "wishlist"):
-        customer = input("Please input the customer ID of the wishlist you wish to see: ")
-        db.customers.find({ 'CustomerId': customer })
-    elif(user_input == "exit"):
+                print("\n ******************************************************* \n")
+                print("BookID: " + str(document.get('bookId')))
+                print("Title: " + str(document.get('title')))
+                print("Genre: " + str(document.get('genre')))
+                print("Author: " + str(document.get('author')))
+                print("\n ******************************************************* \n")
+    elif (user_input == "wishlist"):
+        customer_input = input(
+            "Please input the customer ID of the wishlist you wish to see: ")
+        # included by Patrick
+        # retrieve an object from the database using the code 1003 because it is the only one that has a wishlist more than two items
+        # customer_found = (db.customers.find_one(
+        #     # the customer ID is an integer, so it must be converted to an integer
+        #     {'customerId': int(customer_input)}
+        # ))
+        # this functions only retrieves the keys in the object in this case should be the customer ID, first name, last name, and wishlist
+        # for wishlist in customer_found:
+        #     print(wishlist)
+        # first retrieve the document with the array of books
+        # in this case the document where the wishlist is located is the customer selected
+        customer_wishlist = db.customers.find_one(
+            {'customerId': int(customer_input)})
+        # then retrieve the array of books from the document
+        selected_books = customer_wishlist[selected_books]
+        # then print the array of books
+        for book in selected_books:
+            print(book)
+        print("nnnnnn")
+        print("nnnnnn")
+        # for document in customer_found:
+        #     print("Customer ID: " + str(document.get('customerId')))
+        #     print("First Name: " + str(document.get('firstName')))
+        #     print("Last Name: " + str(document.get('lastName')))
+        #     print("Wishlist: " + str(document.get('wishlist')))
+        # end of included by Patrick
+        # included by Patrick
+    elif (user_input == "customers"):
+        print("Here is a list of our customers: ")
+        customerList = db.customers.find({})
+        for document in customerList:
+            print("\n ******************************************************* \n")
+            print("Customer ID: " + str(document.get('customerId')))
+            print("First Name: " + str(document.get('firstName')))
+            print("Last Name: " + str(document.get('lastName')))
+            print("\n ******************************************************* \n")
+        # end of included by Patrick
+    elif (user_input == "exit"):
         print("Thank you for using the What-a-book app! Good-bye!")
         run_app = False
     else:
         print("Unknown input. Please try again.")
         continue
-
---
+++ console.py
@@ -0,0 +1,77 @@
+# Import statements
+import pymongo
+from pymongo import MongoClient
+
+# Establish a connection to the server
+client = MongoClient(
+    "mongodb+srv://web335_user:s3cret@bellevueuniversity.5jww2it.mongodb.net/")
+
+# Connect to the database
+db = client['web335DB']
+
+# Display all documents in the books collection
+
+
+def display_books(books):
+    print("List of Books:")
+    for book in books:
+        print(
+            f"{book['bookId']}: {book['title']} by {book['author']} - Genre: {book['genre']}")
+    print("\n")
+
+# Get a list of all genres in the books collection
+
+
+def get_genres(books):
+    genres = set()
+    for book in books:
+        genres.add(book['genre'])
+    return list(genres)
+
+# Get all books in a given genre
+
+
+def get_books_by_genre(genre):
+    return db.books.find({"genre": genre})
+
+# Get a customer's wishlist
+
+
+def get_customer_wishlist(customer_id):
+    customer = db.customers.find_one({"customerId": customer_id})
+    if customer:
+        return db.books.find({"bookId": {"$in": [item["bookId"] for item in customer["wishlist"]]}})
+    else:
+        return None
+
+
+# Display all books
+books = db.books.find()
+display_books(books)
+
+# Display all genres
+genres = get_genres(db.books.find())
+print("Genres:")
+for i, genre in enumerate(genres, 1):
+    print(f"{i}. {genre}")
+
+# Display all books in a given genre
+selected_genre = int(
+    input("\nEnter the number of the genre you want to display books for: ")) - 1
+if 0 <= selected_genre < len(genres):
+    books_by_genre = get_books_by_genre(genres[selected_genre])
+    display_books(books_by_genre)
+else:
+    print("Invalid genre selection.\n")
+
+# Display a customer's wishlist
+customer_id = input("Enter a customerId (e.g., c1, c2, or c3): ")
+wishlist = get_customer_wishlist(customer_id)
+if wishlist:
+    print(f"Wishlist for Customer {customer_id}:")
+    display_books(wishlist)
+else:
+    print("Invalid customerId.\n")
+
+# Close the connection
+client.close()
